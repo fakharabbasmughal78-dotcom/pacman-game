e4c2b7677010b0a4caf2eeafd9295611a99a25d8
fakharabbasmughal78-dotcom <fakharabbasmughal78@gmail.com>
2026-04-25 20:13:39 +0500

    Added Pac-Man yellow circle on screen

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
-#include <SFML/Graphics.hpp>
+#include<SFML/Graphics.hpp>
+#include<iostream>
+using namespace std;
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Pac-Man Game");
+class PacMan{
+    public:
+    sf::CircleShape shape;
+    float x,y,speed;
+};
+int main(){
+    sf :: RenderWindow window(sf::VideoMode(800,600),"Pac-Man");
+    PacMan pacman;
+    pacman.shape.setRadius(50);
+    pacman.shape.setFillColor(sf::Color::Yellow);
+    pacman.x=400;
+    pacman.y=300;
+    pacman.speed=0.1;
+    pacman.shape.setPosition(pacman.x , pacman.y);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-        window.display();
+    while(window.isOpen()){
+         sf::Event event;
+         while(window.pollEvent(event)){
+            if(event.type == sf:: Event :: Closed)
+            window.close();
+         }
+         window.clear(sf::Color::Black);
+         window.draw(pacman.shape);
+         window.display();
     }
-
     return 0;
 }
