47abe7e04defcd02d844c183ca59b6ebf3284200
fakharabbasmughal78-dotcom <fakharabbasmughal78@gmail.com>
2026-04-26 20:44:54 +0500

    Added GHost Ai On screen and game over or you can say window close feature today on my first project on 26April started the game on 25 April

--- main.cpp
+++ main.cpp
@@ -8,21 +8,39 @@ class Dot{
     sf::CircleShape shape;
     bool active;
 };
-
-class PacMan{
+class Entity{
     public:
-    sf::CircleShape shape;
+    sf :: CircleShape shape;
     float x,y,speed;
+};
+
+class PacMan : public Entity{
+    public:
+    int lives;
+};
+class Ghost : public Entity {
+ public:
+ float dirX, dirY;
 };
 int main(){
     sf :: RenderWindow window(sf::VideoMode(800,600),"Pac-Man");
     PacMan pacman;
+    pacman.lives=3;
     
     pacman.shape.setRadius(20);
-    pacman.shape.setFillColor(sf::Color::Red);
+    pacman.shape.setFillColor(sf::Color::Yellow);
     pacman.x=300;
     pacman.y=400;
-    pacman.speed=1.3;
+    pacman.speed=1.5;
+     
+    Ghost ghost;
+    ghost.shape.setRadius(20);
+    ghost.shape.setFillColor(sf::Color::Red);
+    ghost.x=100;
+    ghost.y=100;
+    ghost.speed=0.08;
+    ghost.dirX=1;
+    ghost.dirY=1;
 
     int size=10;
     int score =0;
@@ -31,9 +49,15 @@ int main(){
 
     sf :: Text scoreText;
     scoreText.setFont(font);
-    scoreText.setCharacterSize(40);
-    scoreText.setFillColor(sf::Color::Red);
+    scoreText.setCharacterSize(30);
+    scoreText.setFillColor(sf::Color::White);
     scoreText.setPosition(10,10);
+
+    sf::Text livesText;
+    livesText.setFont(font);
+    livesText.setCharacterSize(30);
+    livesText.setFillColor(sf::Color::White);
+    livesText.setPosition(650,10);
 
     Dot dots[size];
     for(int i=0;i<size;i++){
@@ -65,6 +89,35 @@ int main(){
           pacman.x=800 - pacman.shape.getRadius()*2;
           if(pacman.y>600-pacman.shape.getRadius()*2)
           pacman.y=600- pacman.shape.getRadius()*2;  
+           
+          ghost.x+=ghost.speed*ghost.dirX;
+          ghost.y+=ghost.speed*ghost.dirY;
+         //  Ghost movement
+         if(pacman.x>ghost.x){
+            ghost.dirX=1;
+         }else{
+            ghost.dirX=-1;
+         }
+         if(pacman.y>ghost.y){
+            ghost.dirY=1;
+         }else{
+            ghost.dirY=-1;
+        }
+
+         float dx = (pacman.x + pacman.shape.getRadius())-(ghost.x + ghost.shape.getRadius());
+         float dy = (pacman.y + pacman.shape.getRadius())-(ghost.y + ghost.shape.getRadius());
+         float distance = sqrt(dx*dx + dy*dy);
+         if(distance<pacman.shape.getRadius()+ghost.shape.getRadius()){
+            pacman.lives--;
+            pacman.x=300;
+            pacman.y=400;
+            ghost.x=100;
+            ghost.y=100;
+         }
+         if(pacman.lives<=0){
+            window.close();
+         }
+         
 
           for(int i=0;i<size;i++){
             if(dots[i].active){
@@ -88,11 +141,11 @@ int main(){
          scoreText.setString("SCORE: "+ to_string(score));
          window.draw(scoreText);
          window.draw(pacman.shape);
+         ghost.shape.setPosition(ghost.x,ghost.y);
+         window.draw(ghost.shape);
+         livesText.setString("LIVES: "+ to_string(pacman.lives));
+         window.draw(livesText);
          window.display();
     }
     return 0;
 }
-// we was trying to create task.json
-// beacuse in this we have to eneter 3 commoneds 
-//cntrl+shft+b didnot work but score wokred with 3 commandds
-// didnot push the score command till now due to electricity.
