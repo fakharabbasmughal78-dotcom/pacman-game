0276a64607a90b0d370a027d392ab6601a349603
fakharabbasmughal78-dotcom <fakharabbasmughal78@gmail.com>
2026-04-26 21:34:21 +0500

    added GAME OVER text

--- main.cpp
+++ main.cpp
@@ -3,36 +3,40 @@
 #include<iostream>
 #include<string>
 using namespace std;
-class Dot{
+// Classes for Dots 
+class Dot{ 
     public:
     sf::CircleShape shape;
     bool active;
 };
+//Base Class 
 class Entity{
     public:
     sf :: CircleShape shape;
     float x,y,speed;
 };
-
+// derived class for PacMan
 class PacMan : public Entity{
     public:
     int lives;
 };
+//Derived Class for Ghost 
 class Ghost : public Entity {
  public:
  float dirX, dirY;
 };
+//Doing MAIN
 int main(){
     sf :: RenderWindow window(sf::VideoMode(800,600),"Pac-Man");
     PacMan pacman;
     pacman.lives=3;
-    
+    //PACMAN Characteristics 
     pacman.shape.setRadius(20);
     pacman.shape.setFillColor(sf::Color::Yellow);
     pacman.x=300;
     pacman.y=400;
     pacman.speed=1.5;
-     
+    //Ghost Characteristics :
     Ghost ghost;
     ghost.shape.setRadius(20);
     ghost.shape.setFillColor(sf::Color::Red);
@@ -44,21 +48,31 @@ int main(){
 
     int size=10;
     int score =0;
-    sf :: Font font;
+    int gameState=0;
+    sf :: Font font; // FOR font
     font.loadFromFile("consola.ttf");
-
+    //FONT Properties for score 
     sf :: Text scoreText;
     scoreText.setFont(font);
     scoreText.setCharacterSize(30);
     scoreText.setFillColor(sf::Color::White);
     scoreText.setPosition(10,10);
-
+    //FONT Properties for LIves
     sf::Text livesText;
     livesText.setFont(font);
     livesText.setCharacterSize(30);
     livesText.setFillColor(sf::Color::White);
     livesText.setPosition(650,10);
+    //Font properties for GameOver :
+    sf::Text gameOvertext;
+    gameOvertext.setFont(font);
+    gameOvertext.setCharacterSize(50);
+    gameOvertext.setFillColor(sf::Color::Red);
+    gameOvertext.setPosition(200,200);
+    gameOvertext.setString("GAME OVER!!!");
+    //FONT Properties for reply Text:
 
+    //Loop for Dots 
     Dot dots[size];
     for(int i=0;i<size;i++){
         dots[i].shape.setRadius(5);
@@ -66,13 +80,14 @@ int main(){
         dots[i].active=true;
         dots[i].shape.setPosition(100 + i*60,400);
     }
-     
+    // While Loop for the game to coontinue 
     while(window.isOpen()){
          sf::Event event;
          while(window.pollEvent(event)){
             if(event.type == sf:: Event :: Closed)
             window.close();
          }
+         //Arrow keys movement 
          if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
           pacman.x-=pacman.speed;//speed will control how much co-ordinates they will move fast or slower.
           if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
@@ -81,6 +96,7 @@ int main(){
           pacman.y-=pacman.speed;
           if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
           pacman.y+=pacman.speed;
+          //PAcman touching walls 
           if(pacman.x<0) 
           pacman.x=0;
           if(pacman.y<0)
@@ -103,7 +119,7 @@ int main(){
          }else{
             ghost.dirY=-1;
         }
-
+         // Ghost maze touching logic:
          float dx = (pacman.x + pacman.shape.getRadius())-(ghost.x + ghost.shape.getRadius());
          float dy = (pacman.y + pacman.shape.getRadius())-(ghost.y + ghost.shape.getRadius());
          float distance = sqrt(dx*dx + dy*dy);
@@ -113,12 +129,12 @@ int main(){
             pacman.y=400;
             ghost.x=100;
             ghost.y=100;
-         }
+         }//if lives finish terminate:
          if(pacman.lives<=0){
-            window.close();
+            gameState=1; // 1 Mean GameOver , 0 mean countinue :
          }
          
-
+          //Loop for dots active + plus score count :
           for(int i=0;i<size;i++){
             if(dots[i].active){
                 float dx=(pacman.x+pacman.shape.getRadius()) - dots[i].shape.getPosition().x;
@@ -130,13 +146,15 @@ int main(){
                 }
             }
           }
-         window.clear(sf::Color::Black);
-         
-         for(int i=0 ;i<size;i++){
+          //LOOP for game is coutinue or not ?
+         window.clear(sf::Color::Black); // this statement is use to reset the trail of previus frames 
+         if(gameState==0){          
+          for(int i=0 ;i<size;i++){
             if(dots[i].active==true){
                 window.draw(dots[i].shape);
-            }
-         }
+             }
+           }                 
+         //Display code:
          pacman.shape.setPosition(pacman.x,pacman.y);
          scoreText.setString("SCORE: "+ to_string(score));
          window.draw(scoreText);
@@ -144,8 +162,12 @@ int main(){
          ghost.shape.setPosition(ghost.x,ghost.y);
          window.draw(ghost.shape);
          livesText.setString("LIVES: "+ to_string(pacman.lives));
-         window.draw(livesText);
-         window.display();
-    }
+         window.draw(livesText);  
+        }
+         if (gameState==1){
+        window.draw(gameOvertext);
+      }  
+          window.display();   
+    }   
     return 0;
 }
